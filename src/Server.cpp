88e3521cfaeb06bc0eecb3224928aba3b676312d
theonewhoknew <theonewhoknew@protonmail.com>
2024-01-24 10:42:50 +0100

    implementamos llamada a getaddrinfo, para mayor facilidad a la hora de crear estructura de direccion, hacemos que use la IP de la maquina con AI_PASSIVE y hacemos que pueda aceptar tanto ipv4 como ipv6 con AF_UNSPEC

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,12 +3,12 @@
 #define _XOPEN_SOURCE_EXTENDED 1
 #include <sys/socket.h>
 #include <netinet/in.h>
-//#include <bits/stdc++.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <bits/stdc++.h>
 #include <string>
 #include <cstring>
 #include <fcntl.h>
-#include <unistd.h>
-#include <sstream>
 
 Server::Server()
 {
@@ -16,18 +16,34 @@ Server::Server()
 	port = 8888;
 }
 
-static int create_socket(int server_fd)
+static void	get_addr_info(struct addrinfo **s_addr)
 {	
-	int	flags;
+	int				status;
+	struct addrinfo hints;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&hints, 0, sizeof (hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	status = getaddrinfo(NULL, "8888", &hints, s_addr);
+	if (status != 0)
+	{
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    	exit(1);
+	}
+}
+
+static int create_s(int server_fd, struct addrinfo *s_addr)
+{	
+	int flags;
+	
+	server_fd = socket(s_addr->ai_family, s_addr->ai_socktype, s_addr->ai_protocol);
 	if (server_fd < 0)
 	{
 		perror("Socket failed");
         close(server_fd);
         exit(EXIT_FAILURE);
 	}
-	/*seteamos el socket como non blocking, es decir, el server no esperará a recibir informacion del cliente*/
 	flags = fcntl(server_fd, F_GETFD, 0);
 	if (flags == -1)
 	{
@@ -44,18 +60,25 @@ static int create_socket(int server_fd)
 	return (server_fd);
 }
 
-static int bind_socket(int server_fd, sockaddr_in addr, int addr_len)
-{
-	if (bind(server_fd, (const sockaddr *) &addr, addr_len) < 0)
+static int bind_s(int server_fd, struct addrinfo *s_addr)
+{	
+	int	yes = 1;
+
+	if (bind(server_fd, s_addr->ai_addr, s_addr->ai_addrlen) < 0)
 	{
 		perror("Bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
 	}
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (yes)) == -1) 
+	{
+    	perror("setsockopt");
+   		exit(1);
+	} 
 	return (server_fd);
 }
 
-static int listen_socket(int server_fd)
+static int listen_s(int server_fd)
 {
 	if (listen(server_fd, 10) < 0)
 	{
@@ -66,13 +89,15 @@ static int listen_socket(int server_fd)
 	return (server_fd);
 }
 
-static void server_loop(int server_fd, sockaddr_in *addr, int *addr_len)
+static void server_loop(int server_fd)
 {
 	int	new_socket;
+	struct sockaddr_storage c_addr;
+    socklen_t c_addr_size;
 
 	while (true)
 	{	
-		new_socket = accept(server_fd, (struct sockaddr *) addr, (socklen_t *) addr_len);
+		new_socket = accept(server_fd, (struct sockaddr *) &c_addr, &c_addr_size);
 		if (new_socket < 0)
 		{
             perror("Accept Failed");
@@ -84,13 +109,14 @@ static void server_loop(int server_fd, sockaddr_in *addr, int *addr_len)
 
 void Server::start()
 {
-	int server_fd = 0;
-	struct sockaddr_in addr;
-	int	addr_len = sizeof(addr);
+	struct addrinfo *s_addr;
+	int s_fd = 0;
 
-	server_fd = create_socket(server_fd); //creamos socket y lo ponemos a non blocking
-	set_sock_addr(&addr, port, ip); // inicializamos estructura de socket
-	bind_socket(server_fd, addr, addr_len);  // hacemos bind
-	listen_socket(server_fd);  // hacemos listen
-	server_loop(server_fd, &addr, &addr_len);  //loop server en el que gestiona peticiones de clientes, pero sin bloquearse, actuando inmediatamente.
+	get_addr_info(&s_addr);
+	s_fd = create_s(s_fd, s_addr);
+	bind_s(s_fd, s_addr);
+	listen_s(s_fd);
+	server_loop(s_fd);
+
+	freeaddrinfo(s_addr);
 }
