3bbbdc9c46facea3533299df82ec39db197c9705
Laura Garcia Arch <laugarci@car6s2.42barcelona.com>
2024-02-14 12:00:48 +0100

    parsing para encontrar datos del config file mejorado

--- inc/ConfFile.hpp
+++ inc/ConfFile.hpp
@@ -1,7 +1,9 @@
 #ifndef CONFFILE_HPP
 #define CONFFILE_HPP
 
-#include<webserv.hpp>
+#include "Colors.hpp"
+#include <webserv.hpp>
+
 
 class ConfFile
 {
@@ -9,19 +11,17 @@ class ConfFile
 		std::string file;
 		std::vector<class Server>	serv_vec;
 		int		fd_count;
-		std::string _ip;
-		std::string _port;
-		std::string _server_name;
 	public:
 		pollfd 	*poll_ptr;
 		int		fd_size;
 		ConfFile(std::string _file);
-		ConfFile();
 		~ConfFile();
 		void	parse_config(); 
-		int		parse_element(std::string& line);
+		int		parse_element(std::string& content, int i);
 		int		countServers(std::string content);
-		std::string findInfo(std::string line, std::string tofind);
+		std::string findInfo(std::string line, std::string tofind, std::string found);
+		void	findIp(Socket& S, std::string newserv);
+		void	check_info();
 		void	print_servers();
 		void	init_serv();
 		void	init_poll();
