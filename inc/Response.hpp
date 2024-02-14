6b0f81d27963fb525a1e2a65b91b3076f74a9899
theonewhoknew <theonewhoknew@protonmail.com>
2024-02-14 13:44:07 +0100

    creamos estructura status line para responses, tambien escribimos pseudocodigo para gestionar en el futuro las tres peticiones principales, GET, POST, DELETE

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -4,16 +4,27 @@
 #include<iostream>
 #include<HeaderHTTP.hpp>
 
+typedef struct
+{
+	std::string	protocl;
+	std::string code;
+	std::string	text;
+	std::string	whole_line;
+}	Status_line;
+
 class Response
 {
 	public:
 		Response(Request &request);
 		~Response();
 
-		std::string	status_line;
+		Status_line	status_line;
 		HeaderHTTP	headers;
 		std::string body;
 		std::string makeResponse(); // devuelve el texto con el formato completo
+		void 	do_get(Request &request);
+		void 	do_post(Request &request);
+		void 	do_delete(Request &request);
 		void	setStatusLine(std::string _status_line);
 		void	setHeader(std::string _header);
 		void	setBody(std::string _body);
