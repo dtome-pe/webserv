aacea6bac228a4a047982925332df76b1a1b8dc8
theonewhoknew <theonewhoknew@protonmail.com>
2024-02-14 13:44:07 +0100

    creamos estructura status line para responses, tambien escribimos pseudocodigo para gestionar en el futuro las tres peticiones principales, GET, POST, DELETE

--- src/Response.cpp
+++ src/Response.cpp
@@ -4,6 +4,12 @@ Response::Response(Request &request)
 {	
 	(void) request;
 	
+	/*if (request.method == "GET")
+		this->do_get();
+	else if (request.method == "POST")
+		this->do_post();
+	else if (request.method == "DELETE")
+		this->do_delete();	*/
 	this->setStatusLine("HTTP/1.1 200 OK");
 	this->setHeader("Server: apache");
 	this->setBody("Hi");
@@ -16,13 +22,13 @@ Response::~Response()
 
 std::string Response::makeResponse()
 {
-	return (this->status_line + this->headers.makeHeader()
+	return (this->status_line.whole_line + this->headers.makeHeader()
 			+ "\r\n" + this->body);
 }
 
 void	Response::setStatusLine(std::string _status_line)
 {
-	this->status_line = _status_line + "\r\n";
+	this->status_line.whole_line = _status_line + "\r\n";
 }
 
 void	Response::setHeader(std::string _header)
