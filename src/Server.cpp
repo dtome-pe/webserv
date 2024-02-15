a3b0b3bf9b72b7829497aec51931579a55541aa9
Laura Garcia Arch <laugarci@car7s2.42barcelona.com>
2024-02-15 15:45:04 +0100

    anadidos setters y getters en server y cambio de host y servers a vectores WIP

--- src/Server.cpp
+++ src/Server.cpp
@@ -13,3 +13,73 @@ void	Server::start()
 		ptr->sock->start(); //inicializamos los datos del socket
 	}
 }
+
+std::string Server::getPort()
+{
+	return (this->port);
+}
+
+std::string Server::getIp()
+{
+	return (this->ip);
+}
+
+std::string Server::getServerName()
+{
+	return (this->server_name);
+}
+
+std::string Server::getErrorPage()
+{
+	return (this->error_page);
+}
+
+std::string Server::getAllowMethods()
+{
+	return (this->methods);
+}
+
+void	Server::setPort(std::string port)
+{
+	this->port = port;
+}
+
+void	Server::setIp(std::string ip)
+{
+	this->ip = ip;
+}
+
+void	Server::setServerName(std::string serverName)
+{
+	this->server_name = serverName;
+}
+
+void	Server::setErrorPage(std::string errorPage)
+{
+	this->error_page = errorPage;
+}
+
+void	Server::setAllowMethods(std::string allow)
+{
+	this->methods = allow;
+}
+
+void	Server::addVServerName(std::string str)
+{
+	vserver_name.push_back(str);
+}
+
+std::vector<std::string>& Server::getVServerName()
+{
+	return (vserver_name);
+}
+
+void	Server::addVPort(std::string str)
+{
+	vports.push_back(str);
+}
+
+std::vector<std::string>& Server::getVPort()
+{
+	return (vports);
+}
