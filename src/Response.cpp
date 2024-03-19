8d920ce145581a1966a75f1aa4c8f43e1ad9d23f
theonewhoknew <theonewhoknew@protonmail.com>
2024-03-19 10:59:23 +0100

    cambiamos logica para construir respuestas, generamos funcion aparte setResponse, que contenga el setStatusLine y setHeaders de cada uno de los codigos HTTP, para facilitar futuras implementaciones del resto de codigos

--- src/Response.cpp
+++ src/Response.cpp
@@ -9,13 +9,15 @@ void Response::do_cgi(Request &request, std::string &path)
     if (pipe(pipe_fd) == -1) 
 	{
         strerror(errno);
-        return do_500();
+        setResponse(500, *this, "", NULL);
+		return ;
     }
 	pid_t	pid = fork();
 	if (pid == -1)
 	{
 		strerror(errno);
-		return do_500();
+		setResponse(500, *this, "", NULL);
+		return ;
 	}
 	if (pid == 0)
 	{
@@ -23,7 +25,8 @@ void Response::do_cgi(Request &request, std::string &path)
 		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
 		{
 			strerror(errno);
-			return do_500();
+			setResponse(500, *this, "", NULL);
+			return ;
 		}
 		close(pipe_fd[1]);
 		execve(path.c_str(), setArgv(path), setEnvp(request, path));
@@ -36,7 +39,8 @@ void Response::do_cgi(Request &request, std::string &path)
 		if (result == -1)
 		{
 			strerror(errno);
-			return do_500();
+			setResponse(500, *this, "", NULL);
+			return ;
 		}
 		else
 		{
@@ -44,7 +48,10 @@ void Response::do_cgi(Request &request, std::string &path)
        		{
             	int exitStatus = WEXITSTATUS(status);
 				if (exitStatus == -1)
-					do_500();
+				{
+					setResponse(500, *this, "", NULL);
+					return ;
+				}
 				else
 				{
 					/* char buffer[4096];
@@ -70,131 +77,61 @@ void Response::do_cgi(Request &request, std::string &path)
 	}
 }
 
-void Response::do_default() // damos default.html si se accede al root del server pero no hay root directive
-{
-	cout << "entra en do default" << endl;
-
-	this->setStatusLine("HTTP/1.1 200 OK");
-	makeDefault(*this, "/default.html");
-}
-
 void Response::do_redirection(Request &request, std::string return_str)
 {
 	cout << "entra en redirection. return str is " << return_str << endl;
 
 	std::string code = return_str.substr(0, return_str.find(" "));
+	cout << "code is: " << code << endl;
+
 	std::string location = return_str.substr(return_str.find(" ") + 1, return_str.length());
 
+	cout << "url is: " << location << endl;
+
+/* 	if (code == location) // solo hay un código
+	{
+		int c;
+		stringstream(code) >> c;
+
+		makeReturnCode(c, *this);
+	} */
 	if (code == "301")
-		do_301(request.ip + ":" + request.port + location);
-}
-
-
-void Response::do_200_get_path(std::string &path)
-{
-	this->setStatusLine("HTTP/1.1 200 OK");
-
-	std::string content = readFileContents(path);
-
-	this->setHeader("Content-Length: " + getLengthAsString(content));
-	this->setBody(content);
-}
-
-void Response::do_200_get_content(std::string &content)
-{
-	this->setStatusLine("HTTP/1.1 200 OK");
-
-	this->setHeader("Content-Length: " + getLengthAsString(content));
-	this->setBody(content);
-}
-
-void Response::do_301(std::string location)
-{
-	cout << "entra en 301" << endl;
-
-	this->setStatusLine("HTTP/1.1 301 Moved Permanently");
-
-	this->setHeader("Location: http://" + location);
-	this->setHeader("Connection: keep-alive");
-}
-
-void Response::do_403()
-{
-	cout << "entra en 403 " << endl;
-	
-	this->setStatusLine("HTTP/1.1 403 Not Allowed");
-	makeDefault(*this, "/403.html");
-}
-
-void Response::do_404()
-{
-	cout << "entra en 404 " << endl;
-	
-	this->setStatusLine("HTTP/1.1 404 Not Found");
-	makeDefault(*this, "/404.html");
-}
-
-void Response::do_405(const Locations *loc)
-{
-	this->setStatusLine("HTTP/1.1 405 Method Not Allowed");
-	
-	std::string allow_header = "Allow: ";			/*allow header obligatorio en caso de mensaje 405 method not allowed
-													donde se informan de metodos aceptados en URL*/
-	if (loc->getMethods()[0] == 1)
-		allow_header += "GET, ";
-	if (loc->getMethods()[1] == 1)
-		allow_header += "POST, ";
-	if (loc->getMethods()[1] == 1)
-		allow_header += "DELETE";
-	this->setHeader(allow_header);
-	makeDefault(*this, "/405.html");
-}
-
-void Response::do_500()
-{
-	cout << "entra en 500 " << endl;
-	
-	this->setStatusLine("HTTP/1.1 500 Internal Server Error");
+		setResponse(301, *this, request.ip + ":" + request.port + location, NULL);
 }
 
 Response::Response(Request &request, const Server *serv, const Locations *loc)
 {
-	
-	if (request.request_line.method == "GET")
-		this->do_get(request, serv, loc);
-}
-
-void Response::do_get(Request &request, const Server *serv, const Locations *loc)
-{
-
 	/*comprobamos el path del request y realizamos comprobaciones pertinentes*/
 	std::string path = getPath(request, serv, loc); // tambien parseamos una posible question query, para conducir a archivo cgi de manera correcta
-	//cout << "resolved path is " << path << endl;
+	cout << "resolved path is " << path << endl;
 	if (path == "none") // no hay root directives, solo daremos una pagina de webserv si se accede al '/', si no 404
 	{
 		if (request.getTarget() == "/")
-			do_default();
+			setResponse(200, *this, readFileContents(getDefaultFile("/default.html")), NULL);
 		else
-			do_404();
+			setResponse(404, *this, "", NULL);
 	}
 	else
 	{
-		if (!checkGood(path))  // si el path que ha resultado no existe, 404
-		{
-			do_404();
-			return ;
-		}
-		if (checkFileOrDir(path) == "dir" && !checkTrailingSlash(path))  // comprobamos si tiene o no trailing slash, nginx hace una redireccion 301 a URL con final slash
-			return do_301(request.ip + ":" + request.port + request.getTarget() + "/");
 		if (!check_method(request.getMethod(), loc)) // si metodo no permitido, 405
 		{
-			this->do_405(loc);
+			setResponse(405, *this, "", loc);
 			return ;
 		}
 		std::string return_str = checkReturn(loc); // luego se comprueban redirecciones
 		if (return_str != "")
 		{
 			do_redirection(request, return_str);
+			return ;
+		}
+		if (!checkGood(path))  // si el path que ha resultado no existe, 404
+		{
+			setResponse(404, *this, "", NULL);
+			return ;
+		}
+		if (checkFileOrDir(path) == "dir" && !checkTrailingSlash(path))  // comprobamos si tiene o no trailing slash, nginx hace una redireccion 301 a URL con final slash
+		{
+			setResponse(301, *this, request.ip + ":" + request.port + request.getTarget() + "/", NULL);
 			return ;
 		}
 		if (checkFileOrDir(path) == "file")
@@ -206,7 +143,7 @@ void Response::do_get(Request &request, const Server *serv, const Locations *loc
 				return ;
 			}
 			else
-				do_200_get_path(path); //sino servimos el recurso de manera normal
+				setResponse(200, *this, readFileContents(path), NULL); //sino servimos el recurso de manera normal
 		}
 		else // si corresponde a un directorio, primero miramos que no haya un index file
 		{
@@ -214,13 +151,13 @@ void Response::do_get(Request &request, const Server *serv, const Locations *loc
 			std::string index_file = findIndex(path, serv, loc); // checquearemos si hay un index directive, para intentar servir archivo index
 			if (index_file != "")
 			{
-				do_200_get_path(index_file);
+				setResponse(200, *this, readFileContents(path), NULL);
 				return ;
 			}
 			if (findIndexHtml(path)) // sino, buscamos un archivo index.html para servir.
 			{
 				path += "index.html";
-				do_200_get_path(path);
+				setResponse(200, *this, readFileContents(path), NULL);
 				return ;
 			}
 			else // sino, comprobamos si tiene autoindex activado para mostrar listado directorio
@@ -228,22 +165,23 @@ void Response::do_get(Request &request, const Server *serv, const Locations *loc
 				if (!loc || !loc->getAutoindex()) // si no tiene autoindex (como solo lo puede tener un location, de momento), devolvemos 403 ya que no esta activado el directorylisting
 											// y no tenemos permiso para coger ningun archivo de directorio
 				{
-					do_403();
+					setResponse(403, *this, "", NULL);;
 					return ;
 				}
 				else
 				{
 					std::string content = generateDirectoryListing(path);
 					if (content == "Error 500") // por si da algun error interno el server.
-						do_500();
+						setResponse(200, *this, "", NULL);
 					else
-						do_200_get_content(content);
+						setResponse(200, *this, content, NULL);
 					return ;
 				}
 			}		
 		}
 	}
 }
+
 
 Response::~Response()
 {
