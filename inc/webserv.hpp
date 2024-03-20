f955258aa6c5ffc7c4d15ca07c643ea42c9adbf3
theonewhoknew <theonewhoknew@protonmail.com>
2024-03-20 12:15:47 +0100

    aislamos Cgi a archivo independiente

--- inc/webserv.hpp
+++ inc/webserv.hpp
@@ -28,28 +28,10 @@ class Server;
 		int		fd_size;
 		int		fd_count;
 	} t_data;
-
-
-
-	void						init_servers(t_data *data, t_serv *lst);
-	void						init_poll(t_data *data, t_serv *lst);
-	void						init_data(t_data *data);
-
-	/*parse config*/
-	int							parse_config(const std::string& file, t_data *data);
-
-	int							parse_listen(t_sock **list, std::string &line);
 	
-	/*start server*/
-	void						get_addr_info(struct addrinfo **s_addr, const char *host, const char *port);
-	int 						create_s(int server_fd, struct addrinfo *s_addr, struct sockaddr_in sock_addr, socklen_t sock_addrlen);
-	int 						bind_s(int server_fd, struct addrinfo *s_addr, std::string ip);
-	int 						listen_s(int server_fd);
-
+	/*find blocks*/
 	bool						look_for_same(Socket &sock, std::vector<Socket>&sock_vec);
-
 	const Server 				*find_serv_block(const std::vector<class Server> &serv, Request &request);
-
 	const Locations 			*find_loc_block(const Server *serv, Request &req);
 
 	/*response utils*/
@@ -71,14 +53,15 @@ class Server;
 	std::string 				getDefaultFile(const std::string &file);
 	void						makeDefault(Response &response, const std::string &file, const Server *serv);
 
-	bool						checkCgi(std::string &path);
+	/*cgi*/
 
-	/*cgi utils*/
+	bool						checkCgi(std::string &path);
+	void						cgi(Response &response, Request &request, std::string &path);
+
 	char* const* 				setEnvp(Request &request, std::string &path);
 	char* const*				setArgv(std::string &path);
 	std::string 				bounceContent(int *pipe_fd);
 	std::string 				getCgiHeader(const std::string& content, const std::string &header);
-	std::string					removeHeaders(std::string &content);
 	void						removeHeaderLine(std::string& content);
 
 	/*poll utils*/
@@ -93,10 +76,6 @@ class Server;
 	/*check GET path*/
 	bool						checkGood(const std::string &path);
 	std::string 				checkFileOrDir(const std::string &path);
-
-	/*poll*/
-	//void	poll_loop(t_data *data);
-	void						poll_loop(std::vector<pollfd> &pollVec, ConfFile &conf);
 
 	/*print utils*/
 	void						print_error(const char *str);
