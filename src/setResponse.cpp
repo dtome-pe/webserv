09e787c1537858ea344273183aeb477af8c1b621
theonewhoknew <theonewhoknew@protonmail.com>
2024-03-20 10:14:16 +0100

    damos un 403 forbidden si se establece index directive, pero el path al camino es incorrecto, para emular a nginx, preparamos para gestionar las paginas de error por defecto, parseadas de la conf file

--- src/setResponse.cpp
+++ src/setResponse.cpp
@@ -1,8 +1,8 @@
 #include <webserv.hpp>
 
-void	setResponse(int code, Response &response, std::string arg, const Locations *loc)
+void	setResponse(int code, Response &response, std::string arg, const Server *serv, const Locations *loc)
 {
-	cout << "code: " << code << "arg: " << arg << endl;
+	cout << "code: " << code << " arg: " << arg << endl;
 	switch (code)
 	{
 		case 200:
@@ -27,13 +27,13 @@ void	setResponse(int code, Response &response, std::string arg, const Locations 
 		case 403:
 		{
 			response.setStatusLine("HTTP/1.1 403 Forbidden");
-			makeDefault(response, "/403.html");
+			makeDefault(response, "/403.html", serv);
 			break;
 		}
 		case 404:
 		{
 			response.setStatusLine("HTTP/1.1 404 Not Found");
-			makeDefault(response, "/404.html");
+			makeDefault(response, "/404.html", serv);
 			break;
 		}
 		case 405:
@@ -48,7 +48,7 @@ void	setResponse(int code, Response &response, std::string arg, const Locations 
 			if (loc->getMethods()[2] == 1)
 				allow_header += "DELETE";
 			response.setHeader(allow_header);
-			makeDefault(response, "/405.html");
+			makeDefault(response, "/405.html", serv);
 			break;
 		}
 	}
