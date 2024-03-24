02398646574006cff86e923dfcb3606ae6a55066
dtome-pe <theonewhoknew@protonmail.com>
2024-03-24 16:10:36 +0100

    update Headers y Request

--- src/HeaderHTTP.cpp
+++ src/HeaderHTTP.cpp
@@ -11,45 +11,41 @@ HeaderHTTP::~HeaderHTTP()
 }
 
 int		HeaderHTTP::setHeader(std::string _header)
-{
-	this->header_vec.push_back(_header);
-	
+{	
+	vec.push_back(_header);
 	int n = _header.find(':');
 	std::string name = _header.substr(0, n);
 	std::string value = _header.substr(n + 2, _header.length());
 	if (name == "Host")
 	{
-		if (this->header_map.find(name) != this->header_map.end())
+		if (this->map.find(name) != this->map.end())
 		{
 			return (1);
 		}
 	}
-	this->header_map[name] = value;
+	this->map[name] = value;
 	return (0);
 	//std::cout << name << ": " << this->headers[name] << std::endl;
 }
 
 std::string	HeaderHTTP::getHeader(std::string name)
 {
-	std::map<std::string, std::string>::iterator it = header_map.begin();
+	std::map<std::string, std::string>::iterator it = map.begin();
 	
-	while (it != header_map.end())
+	while (it != map.end())
 	{
 		if (it->first == name)
 			return it->second;
 		it++;
 	}
-	/* for (size_t i = 0; i < this->header_vec.size(); i++)
-		if (this->header_vec[i].substr(0, this->header_vec[i].find(':') - 1) == name)
-			return (this->header_vec[i]); */
 	return (NULL);
 }
 
 std::string	HeaderHTTP::makeHeader()
 {
 	std::string text;
-	for (size_t i = 0; i < this->header_vec.size(); i++)
-		text += this->header_vec[i] + "\r\n";
+	for (size_t i = 0; i < vec.size(); i++)
+		text += vec[i] + "\r\n";
 	return (text);
 }
 
@@ -80,9 +76,9 @@ std::vector<std::string> HeaderHTTP::split(const std::string& input, const std::
 
 void	HeaderHTTP::printHeaders()
 {	
-	std::map<std::string, std::string>::iterator it = header_map.begin();
+	std::map<std::string, std::string>::iterator it = map.begin();
 
-	while (it != header_map.end())
+	while (it != map.end())
 	{
 		cout << "Header: " << it->first << " Value: " << it->second
 			<< endl;
