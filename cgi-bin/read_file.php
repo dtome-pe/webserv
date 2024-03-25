1fe910bab719388d742a93a0932f3455cb72a594
dtome-pe <theonewhoknew@protonmail.com>
2024-03-25 01:35:23 +0100

    seteamos envp para cgi sin espacios, y realizamos post basico subiendo un archivo y imprimiendo en pantalla el contenido, con read_file.html

--- cgi-bin/read_file.php
+++ cgi-bin/read_file.php
@@ -1,23 +1,36 @@
 <?php
+echo("Content-type: text/html\n");
 
-// Check if POST data is received
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>Received POST Data</title>";
+echo "</head>";
+echo "<body>";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if CONTENT_LENGTH is set
-        echo "test\n";
     if (isset($_SERVER['CONTENT_LENGTH'])) {
         // Read the specified number of bytes from stdin
         $contentLength = (int)$_SERVER['CONTENT_LENGTH'];
         $postData = fread(STDIN, $contentLength);
 
         // Print out the POST data
-        echo "Received POST data:\n";
-        echo $postData;
+        echo "<h1>Received POST data:</h1>";
+        echo "<pre>";
+        echo htmlspecialchars($postData); // Convert special characters to HTML entities to prevent XSS attacks
+        echo "</pre>";
     } else {
         // If CONTENT_LENGTH is not set, return an error
-        echo "CONTENT_LENGTH not set.";
+        echo "<h1>ERROR:</h1>";
+        echo "<p>CONTENT_LENGTH not set.</p>";
     }
 } else {
     // If not a POST request, return an error
-    echo "Only POST requests are supported.";
+    echo "<h1>ERROR:</h1>";
+    echo "<p>Only POST requests are supported.</p>";
 }
+
+echo "</body>";
+echo "</html>";
 ?>
--- inc/Locations.hpp
+++ inc/Locations.hpp
@@ -10,36 +10,36 @@
 
 class Locations {
 	private:
-		bool autoindex;
-		std::string location;
+		bool 									autoindex;
+		std::string 							location;
 //		std::string index;
-		int methods[4]; //[0] = GET, [1] = POST, [2] = DELETE. [3] = PUT. Si es 0 no permitido si es 1 permitido.
-		std::string redirection;
-		int	redirectNum;
-		std::string root;
-		std::string allurl;
-		std::map<std::string, std::string> cgi;
-		std::vector<std::string> index;
+		int 									methods[4]; //[0] = GET, [1] = POST, [2] = DELETE. [3] = PUT. Si es 0 no permitido si es 1 permitido.
+		std::string 							redirection;
+		int										redirectNum;
+		std::string 							root;
+		std::string 							allurl;
+		std::map<std::string, std::string> 		cgi;
+		std::vector<std::string> 				index;
 	public:
 		Locations();
 		~Locations();
-		void setAutoindex(bool autoin);
-		void setVIndex(std::vector<std::string> idx);
-		void setMethods(int met[4]);
-		void setLocation(std::string loc);
-		void setRedirection(std::string red);
-		void setRoot(std::string rt);
-		void setAllUrl(std::string url);
-		void setCGI(std::string ext, std::string path);
-		std::map<std::string, std::string> getCGI() const;
-		bool getAutoindex() const;
-		std::vector<std::string> getIndex() const;
-		const int *getMethods() const;
-		const std::string getLocation() const;
-		const std::string getRedirection() const;
-		int getRedirectionNumber() const;
-		const std::string getRoot() const;
-		std::string getAllUrl();
+		void 									setAutoindex(bool autoin);
+		void									setVIndex(std::vector<std::string> idx);
+		void 									setMethods(int met[4]);
+		void 									setLocation(std::string loc);
+		void 									setRedirection(std::string red);
+		void 									setRoot(std::string rt);
+		void 									setAllUrl(std::string url);
+		void 									setCGI(std::string ext, std::string path);
+		std::map<std::string, std::string> 		getCGI() const;
+		bool 									getAutoindex() const;
+		std::vector<std::string> 				getIndex() const;
+		const int 								*getMethods() const;
+		const std::string 						getLocation() const;
+		const std::string 						getRedirection() const;
+		int 									getRedirectionNumber() const;
+		const std::string 						getRoot() const;
+		std::string 							getAllUrl();
 };
 
 #endif
