c62d428e6c4112225d7fbd27686d2624a0154ff9
dtome-pe <theonewhoknew@protonmail.com>
2024-04-01 12:05:02 +0200

    corregimos error en content-type, tambien corregimos logica para read_file.html/php

--- cgi-bin/read_file.php
+++ cgi-bin/read_file.php
@@ -1,36 +1,18 @@
 <?php
-echo("Content-type: text/html\n");
+$fileContents = file_get_contents($_FILES['file']['tmp_name']);
 
-echo "<!DOCTYPE html>";
-echo "<html>";
-echo "<head>";
-echo "<title>Received POST Data</title>";
-echo "</head>";
-echo "<body>";
+$body = "<!DOCTYPE html>
+<html>
+<head>
+<title>Received POST Data</title>
+</head>
+<body>
+<h1>Info passed by stdin was:</h1>
+<p>$fileContents</p>
+</body>
+</html>";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if CONTENT_LENGTH is set
-    if (isset($_SERVER['CONTENT_LENGTH'])) {
-        // Read the specified number of bytes from stdin
-        $contentLength = (int)$_SERVER['CONTENT_LENGTH'];
-        $postData = fread(STDIN, $contentLength);
+header("Content-type: text/html; charset=UTF-8");
 
-        // Print out the POST data
-        echo "<h1>Received POST data:</h1>";
-        echo "<pre>";
-        echo htmlspecialchars($postData); // Convert special characters to HTML entities to prevent XSS attacks
-        echo "</pre>";
-    } else {
-        // If CONTENT_LENGTH is not set, return an error
-        echo "<h1>ERROR:</h1>";
-        echo "<p>CONTENT_LENGTH not set.</p>";
-    }
-} else {
-    // If not a POST request, return an error
-    echo "<h1>ERROR:</h1>";
-    echo "<p>Only POST requests are supported.</p>";
-}
-
-echo "</body>";
-echo "</html>";
+echo $body;
 ?>
