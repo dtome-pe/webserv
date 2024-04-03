2e6d91c7d9e5815c17418fa6df38c67998e9320a
dtome-pe <theonewhoknew@protonmail.com>
2024-04-03 20:27:50 +0200

    cambiamos script delete a python, cambiamos gestion de retorno de CGI: si cgi devuelve header status, establecemos ese codigo como respuesta en setResponse y luego borramos header Status ya que no es propio de las respuestas

---
+++ cgi-bin/delete.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import shutil
+
+print ("Content-type: text/html; charset=UTF-8")
+
+if not os.path.exists(sys.argv[1]):
+    print("Status: 404 Not Found")
+
+if os.path.isfile(sys.argv[1]):
+    os.unlink(sys.argv[1])
+    print ("Status: 204 No Content")
+
+elif (os.path.isdir(sys.argv[1])):
+    shutil.rmtree(sys.argv[1])
+    print ("Status: 204 No Content")
+
+sys.exit(0)
