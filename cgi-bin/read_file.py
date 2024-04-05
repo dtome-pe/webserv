7d4667dd26aeab2098acc057c851ed5c7843dca2
dtome-pe <theonewhoknew@protonmail.com>
2024-04-05 09:40:27 +0200

    adecuamos header location en respuestas 201, se muestra archivo creado en path que marca upload_store

---
+++ cgi-bin/read_file.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+fileContent = sys.stdin.read()
+
+print ("Content-type: text/html; charset=UTF-8")
+
+print()
+
+print ('''<!DOCTYPE html>
+<html>
+<head>
+<title>Read FILE</title>
+</head>
+<body>
+<h1>File content is:</h1>
+<p> ''')
+print (fileContent)
+print ('''</p>
+</body>
+</html>''')
+
