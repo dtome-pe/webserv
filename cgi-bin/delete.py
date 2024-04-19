a98dec9d8269f2c2588cf575ee1b98ad3fc5584b
dtome-pe <theonewhoknew@protonmail.com>
2024-04-19 20:59:42 +0200

    protegemos scripts de cgi con un try except, para que a cualquier error no previsto, pasando comando curl con opciones no implementadas en el script, se devuelva un 500 internal server error

--- cgi-bin/delete.py
+++ cgi-bin/delete.py
@@ -3,16 +3,18 @@ import os
 import sys
 import shutil
 
-print ("Content-Type: text/html; charset=UTF-8")
+try:
+    if not os.path.exists(sys.argv[1]):
+        print("Status: 404 Not Found")
 
-if not os.path.exists(sys.argv[1]):
-    print("Status: 404 Not Found")
+    if os.path.isfile(sys.argv[1]):
+        os.unlink(sys.argv[1])
+        print("Status: 204 No Content")
 
-if os.path.isfile(sys.argv[1]):
-    os.unlink(sys.argv[1])
-    print("Status: 204 No Content")
+    elif (os.path.isdir(sys.argv[1])):
+        shutil.rmtree(sys.argv[1])
+        print("Status: 204 No Content")
 
-elif (os.path.isdir(sys.argv[1])):
-    shutil.rmtree(sys.argv[1])
-    print("Status: 204 No Content")
+except Exception as e:
+    print("Status: 500 Internal Server Error")
 
